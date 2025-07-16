d6bde4d2ef4c7cd3e90c5ef0a986cb417d942e5a
EkaterineGorgadze <ekaterinegorgadze959@gmail.com>
2025-07-16 18:20:43 +0400

    created a new branch for Customer registry

--- CustomerRegistry.cs
+++ CustomerRegistry.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace BankingAppV01
 {
      public class CustomerRegistry
      {
-          
+          private List<Customer> Customers = new List<Customer>();
+          public CustomerRegistry()
+          {
+
+          }
+
+          public registerCustomer(Customer customer)
+          {
+               
+          }
      }
 }
